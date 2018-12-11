60ffc3609f825ef82c989f029e887055eeca396d
Brandon Makin <bmicmak@gmail.com>
2018-12-10 19:24:20 -0800

    added level terrain back in

--- gyro_godot_project/levels/level 01/InterpolatedCamera.gd
+++ gyro_godot_project/levels/level 01/InterpolatedCamera.gd
@@ -9,6 +9,4 @@ func _ready():
 		world = $"../Level 1"
 	elif id == 2:
 		world = $"../../../ViewportContainer1/Viewport1/Level 1"
-
-func _process(delta):
 	set_target(world.get_node("vehicles/vehicle%s/CamTarget" % id))
