441a6e55ab09bab7ef4af6938db3f95cb3c3e1e6
Brandon Makin <bmicmak@gmail.com>
2019-03-12 14:01:39 -0700

    first steps towards a TCP server

--- gyro_godot_project/scripts/Startup Remote.gd
+++ gyro_godot_project/scripts/Startup Remote.gd
@@ -5,58 +5,67 @@ and exists for the entirety of the game.
 
 extends HTTPRequest
 
-var static_url_and_port = "http://ec2-54-193-74-3.us-west-1.compute.amazonaws.com:8000"
+var static_server_url = "ec2-54-193-74-3.us-west-1.compute.amazonaws.com"
+var server_port = 8000
+#var static_url_and_port = "http://ec2-54-193-74-3.us-west-1.compute.amazonaws.com:8000"
 #var static_url_and_port = "http://localhost:8000"
 var is_remote = true
 var players = []
-var port = 8001
-var udp = PacketPeerUDP.new()
+#var port = 8001
+#var udp = PacketPeerUDP.new()
+var tcp = StreamPeerTCP.new()
 var server_pid
 var qr
 var DEADZONE_RADIUS = 0.05
 
 func _ready():
-	var err = udp.listen(port)
+#	var err = udp.listen(port)
+
+	var err = tcp.connect_to_host(static_server_url, server_port)
 	if err != OK:
-		print("Can't listen specified port")
-	
-	request(static_url_and_port + "/start")
+		print("Can't connect to the remote server via TCP")
+#
+#	request(static_url_and_port + "/start")
 	
 func _process(delta):
 	
-	while(udp.get_available_packet_count() > 0):
-		var packet = udp.get_packet().get_string_from_utf8()
-		var code = packet.left(1)
-		packet = packet.right(1)
-		print("packet: " + str(packet))
-		match code:
-			"9":
-				#print("pong")
-				request(static_url_and_port + "/pong")
-			"8":
-				qr = packet
-#				print(packet)
-				print(qr)
-#			"0":
-#				get_tree().call_group("messengers", "_on_message", packet)
-			"1": # On player connect
-				players.append(packet)
-				print("Global - players: " + str(players))
-				get_tree().call_group("messengers", "_on_connect", packet)
-			"2": # On player disconnect
-				players.remove(players.find(packet))
-				printerr(players)
-				get_tree().call_group("messengers", "_on_disconnect", packet)
-			"3": # On player phone button press
-				var data = JSON.parse(packet).result #data contains i (id), n (name), and s (state)
-				get_tree().call_group("messengers", "_on_button", data.i, data.n, data.s)
-			"4": # On player phone rotation
-				var data = JSON.parse(packet).result #data contains id, a (angle), and t (tilt)
-				var angle = clean_angle_data(data.a)
-				var tilt = clean_tilt_data(data.t)
-				get_tree().call_group("messengers", "_on_rotate", data.id, angle, tilt)
-			_:
-				print("Unknown message: " + packet)
+	while(tcp.get_available_bytes()) > 0:
+		print(tcp.get_utf8_string(tcp.get_available_bytes()))
+		
+	
+#	while(udp.get_available_packet_count() > 0):
+#		var packet = udp.get_packet().get_string_from_utf8()
+#		var code = packet.left(1)
+#		packet = packet.right(1)
+#		print("packet: " + str(packet))
+#		match code:
+#			"9":
+#				#print("pong")
+#				request(static_url_and_port + "/pong")
+#			"8":
+#				qr = packet
+##				print(packet)
+#				print(qr)
+##			"0":
+##				get_tree().call_group("messengers", "_on_message", packet)
+#			"1": # On player connect
+#				players.append(packet)
+#				print("Global - players: " + str(players))
+#				get_tree().call_group("messengers", "_on_connect", packet)
+#			"2": # On player disconnect
+#				players.remove(players.find(packet))
+#				printerr(players)
+#				get_tree().call_group("messengers", "_on_disconnect", packet)
+#			"3": # On player phone button press
+#				var data = JSON.parse(packet).result #data contains i (id), n (name), and s (state)
+#				get_tree().call_group("messengers", "_on_button", data.i, data.n, data.s)
+#			"4": # On player phone rotation
+#				var data = JSON.parse(packet).result #data contains id, a (angle), and t (tilt)
+#				var angle = clean_angle_data(data.a)
+#				var tilt = clean_tilt_data(data.t)
+#				get_tree().call_group("messengers", "_on_rotate", data.id, angle, tilt)
+#			_:
+#				print("Unknown message: " + packet)
 
 func clean_angle_data(angle):
 	# PRECONDITION: angle ranges from -90 to 90
