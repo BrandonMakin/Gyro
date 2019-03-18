3f33b6ee9c3f5e398a9e81bb4bb48d3de28c28b8
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-03-18 19:52:18 -0400

    Quadriatic interpolation on camera FOV

--- gyro_godot_project/levels/level 01/InterpolatedCamera.gd
+++ gyro_godot_project/levels/level 01/InterpolatedCamera.gd
@@ -28,4 +28,4 @@ func _process(delta):
 	# Increase fov as you go faster.
 	# NOTE that it uses speed_level, which isn't guaranteed to be linearly proportional to the player's actual speed
 	# For fov to be exactly proportional to the actual speed, lerp by something like target_vehicle.speed_level / target_vehicle.max_speed, probably.
-	fov = lerp(min_speed_FOV, max_speed_FOV, target_vehicle.speed_level) 
+	fov = lerp(min_speed_FOV, max_speed_FOV, target_vehicle.speed_level * target_vehicle.speed_level) 
