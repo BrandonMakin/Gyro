89081083540a30e92a3b2832b369a350efc8bf92
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-03-26 19:33:03 -0700

    InterpolatedCamera no longer enabled as Interpolated

--- gyro_godot_project/levels/level 01/InterpolatedCamera.gd
+++ gyro_godot_project/levels/level 01/InterpolatedCamera.gd
@@ -12,18 +12,17 @@ var max_speed_FOV : int = 70
 
 
 func _ready():
-	#Utilizing InterpolatedCamera's internal values to track the camera offset of the vehicle
-	enabled = true
-	speed = 10
 	world = $"../../../../Level/Level 1"
 	#Camera target is dependent on camera ID
 	target_vehicle = world.get_node("vehicles/vehicle%s" % id)
 	set_target(world.get_node("vehicles/vehicle%s/CamTarget" % id))
 	cam_offset = get_node(target).transform.origin
 	#Sets initial camera position
-	global_transform.origin = get_node(target).get_parent().global_transform.origin + cam_offset
+
 
 func _process(delta): 
+	global_transform.origin = get_node(target).get_parent().global_transform.origin + cam_offset
+	cam_offset = cam_offset.linear_interpolate(get_node(target).global_transform.origin - get_node(target).get_parent().global_transform.origin, lerp_amount * delta)
 	# Increase fov as you go faster.
 	# NOTE that it uses speed_level, which isn't guaranteed to be linearly proportional to the player's actual speed
 	# For fov to be exactly proportional to the actual speed, lerp by something like target_vehicle.speed_level / target_vehicle.max_speed, probably.
