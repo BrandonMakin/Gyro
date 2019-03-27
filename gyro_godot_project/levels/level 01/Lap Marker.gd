c2c9c226bc45e35d816ed322d9f0a8efeb781515
Brandon Makin <bmicmak@gmail.com>
2019-03-27 14:43:32 -0700

    hey now we got a racing loop

--- gyro_godot_project/levels/level 01/Lap Marker.gd
+++ gyro_godot_project/levels/level 01/Lap Marker.gd
@@ -12,7 +12,7 @@ func _ready():
 func _on_Lap_Tracker_00_body_entered(body):
 	if body.is_in_group("vehicles"):
 		if vehicle_progress_in_lap[body] == 02:
-			vehicle_laps_completed[body] += 1
+			lap_completed(body)
 			print("lap completed. on lap #" + str(vehicle_laps_completed[body]))
 		
 		vehicle_progress_in_lap[body] = 00
@@ -26,3 +26,37 @@ func _on_Lap_Tracker_02_body_entered(body):
 	if body.is_in_group("vehicles"):
 		if vehicle_progress_in_lap[body] == 01:
 			vehicle_progress_in_lap[body] = 02
+
+func lap_completed(body):
+	vehicle_laps_completed[body] += 1
+	var laps = vehicle_laps_completed[body]
+	if body.get_name() == "vehicle1":
+		if laps == 2:
+			$"../GUI/LapMessages/GUI/Lap2p1".visible = true
+			yield(get_tree().create_timer(.5), "timeout") # wait for 0.5s
+			$"../GUI/LapMessages/GUI/Lap2p1".visible = false
+		elif laps == 3:
+			$"../GUI/LapMessages/GUI/Lap3p1".visible = true
+			yield(get_tree().create_timer(.5), "timeout") # wait for 0.5s
+			$"../GUI/LapMessages/GUI/Lap3p1".visible = false
+		elif laps == 4:
+			$"../GUI/LapMessages/race_finished_p1".visible = true
+			yield(get_tree().create_timer(4), "timeout") # wait for 0.5s
+			$"../GUI/LapMessages/race_finished_p1".visible = false
+			
+	elif body.get_name() == "vehicle2":
+		if laps == 2:
+			$"../GUI/LapMessages/GUI/Lap2p2".visible = true
+			yield(get_tree().create_timer(.5), "timeout") # wait for 0.5s
+			$"../GUI/LapMessages/GUI/Lap2p2".visible = false
+		elif laps == 3:
+			$"../GUI/LapMessages/GUI/Lap3p2".visible = true
+			yield(get_tree().create_timer(.5), "timeout") # wait for 0.5s
+			$"../GUI/LapMessages/GUI/Lap3p2".visible = false
+		elif laps == 4:
+			$"../GUI/LapMessages/race_finished_p1".visible = true
+			yield(get_tree().create_timer(4), "timeout") # wait for 0.5s
+			$"../GUI/LapMessages/race_finished_p1".visible = false
+		
+	else:
+		printerr("Lap Marker.gd: Unkown vehicle with name: " + body.get_name())
--- gyro_godot_project/levels/level 01/StoplightAnimationPlayer.gd
+++ gyro_godot_project/levels/level 01/StoplightAnimationPlayer.gd
@@ -9,5 +9,5 @@ func _ready():
 	play("Stoplight")
 
 func let_the_fish_swim():
-	for fish in $"../../vehicles".get_children():
+	for fish in $"../../../vehicles".get_children():
 		fish.change_state("Swimming")
