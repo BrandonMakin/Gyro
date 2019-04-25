e2de9fe38f5d44195cb6e868fec4599749fb5270
Brandon Makin <bmicmak@gmail.com>
2019-04-24 18:50:17 -0700

    added real-time rank placement

--- gyro_godot_project/levels/level 01/Lap Marker.gd
+++ gyro_godot_project/levels/level 01/Lap Marker.gd
@@ -1,25 +1,34 @@
 extends Spatial
 
 var vehicles = {}
+var placements = []
 var players_finished = 0
 onready var vehicle_count = $"../vehicles".get_child_count()
 onready var splitscreen = $"../SplitscreenCamera"
 
-var vehicle_progress_in_lap = {}
-var vehicle_laps_completed = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for vehicle in $"../vehicles".get_children():
-		vehicle_laps_completed[vehicle] = 1
-		vehicle_progress_in_lap[vehicle] = 00
-		
 	for i in range(vehicle_count):
+		placements.append(i)
 		vehicles[i] = {}
 		vehicles[i].finished = false
-#		vehicles[i].current_lap = 1
-#		vehicles[i].progress_in_lap = 0
-		
+		vehicles[i].current_lap = 1
+		vehicles[i].progress_in_lap = 0
+		vehicles[i].rank = 0
+	
+	# connect all the children's body_entered to _on_Lap_Tracker_body_entered
+	for i in range(get_child_count()):
+		if i == 0:
+			continue # skip the Lap Tracker #0 because it's a special case.
+		# connect the signal to the function and bind i as an extra argument. 
+#warning-ignore:return_value_discarded
+		get_child(i).connect("body_entered", self, "_on_Lap_Tracker_body_entered", [i])
+
+func _process(delta):
+	placements.sort_custom(self, "sort")
+	for i in range(placements.size()):
+		splitscreen.get_cam_gui(placements[i]).get_node("Lap/Inside/Label").text = str(i+1)
 
 func race_start():
 	for i in range(vehicle_count):
@@ -43,26 +52,25 @@ func race_finish(id): # id == VehicleNumber
 
 func _on_Lap_Tracker_00_body_entered(body):
 	if body.is_in_group("vehicles"):
-		if vehicle_progress_in_lap[body] == 02:
+		var id = body.vehicle_number
+		if vehicles[id].progress_in_lap == get_child_count() - 1:
 			lap_completed(body)
-			print("lap completed. on lap #" + str(vehicle_laps_completed[body]))
+#			print("lap completed on lap #" + str(vehicles[id].current_lap))
 		
-		vehicle_progress_in_lap[body] = 00
+		vehicles[id].progress_in_lap = 0
 
-func _on_Lap_Tracker_01_body_entered(body):
+func _on_Lap_Tracker_body_entered(body, tracker_number):
 	if body.is_in_group("vehicles"):
-		if vehicle_progress_in_lap[body] == 00:
-			vehicle_progress_in_lap[body] = 01
-
-func _on_Lap_Tracker_02_body_entered(body):
-	if body.is_in_group("vehicles"):
-		if vehicle_progress_in_lap[body] == 01:
-			vehicle_progress_in_lap[body] = 02
+		var id = body.vehicle_number
+#		print("previous tracker number: " + str(vehicles[id].progress_in_lap))
+#		print("previous == expected: " + str(vehicles[id].progress_in_lap == tracker_number-1))
+		if vehicles[id].progress_in_lap == tracker_number-1:
+			vehicles[id].progress_in_lap = tracker_number
 
 func lap_completed(body):
 	var id = body.vehicle_number
-	vehicle_laps_completed[body] += 1
-	var laps = vehicle_laps_completed[body]
+	vehicles[id].current_lap += 1
+	var laps = vehicles[id].current_lap
 	if laps == 2:
 		splitscreen.get_cam_gui(id).get_node("Lap/Inside/Label").text = "Lap 2/3"
 		splitscreen.get_cam_gui(id).get_node("LapAlerts/Lap2").visible = true
@@ -75,3 +83,14 @@ func lap_completed(body):
 		splitscreen.get_cam_gui(id).get_node("LapAlerts/Lap3").visible = false
 	elif laps == 4:
 		race_finish(id)
+
+func sort(id1, id2):
+	if vehicles[id1].progress_in_lap != vehicles[id2].progress_in_lap:
+		return vehicles[id1].progress_in_lap > vehicles[id2].progress_in_lap
+	else:
+		# find who is closest to next lap marker
+		var next_lap_marker = get_child(vehicles[id1].progress_in_lap + 1)
+		var marker_pos = next_lap_marker.translation
+		var dist1 = $"../vehicles".get_child(id1).translation.distance_to(marker_pos)
+		var dist2 = $"../vehicles".get_child(id1).translation.distance_to(marker_pos)
+		return dist1 > dist2
